2fef4db5a435c46e3401e4a205ce794bfafbb9e5
yeahieakhan <yeahieakhan2017@gmail.com>
2026-02-16 17:37:30 +0600

    start

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+const app = express()
+const port = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World Form next level!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
