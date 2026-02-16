078f866d54fd6da000f10db6b33775d5a95ee245
yeahieakhan <yeahieakhan2017@gmail.com>
2026-02-16 17:53:25 +0600

    stating express js

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,26 @@ import express, { Request, Response } from "express";
 const app = express()
 const port = 5000;
 
+
+//cookie parser
+app.use(express.json());
+
+// form data handing
+// app.use(express.urlencoded());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World Form next level!')
 })
+
+app.post("/", (req: Request, res: Response) => {
+    console.log(req.body);
+    res.status(201).json({
+        success: true,
+        message: "API is working",
+    })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
