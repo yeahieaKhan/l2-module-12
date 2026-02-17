7db44938746ba2f9ab14e249b5698913797fc245
yeahieakhan <yeahieakhan2017@gmail.com>
2026-02-17 14:19:27 +0600

    connect with db with pool

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,40 @@ import express, { Request, Response } from "express";
 import { Pool } from "pg";
 const app = express()
 const port = 5000;
-
-
-const pool = new Pool({
-    connectionString: `postgresql://neondb_owner:npg_hvOm29VlBFxM@ep-crimson-thunder-aig08dr7-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
-});
-
-
 //cookie parser
 app.use(express.json());
 
 // form data handing
 // app.use(express.urlencoded());
+
+
+const pool = new Pool({
+  connectionString: `postgresql://
+    neondb_owner:npg_hvOm29VlBFxM@ep-crimson-thunder-aig08dr7-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+});
+
+
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IS NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(1500) UNIQUE NOT NULL,
+    age INT,
+    phone VARCHAR(100),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    )
+    `
+
+  )
+}
+
+
+initDB();
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World Form next level!')
