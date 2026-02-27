1458e8d6ebd8d7d327410c31fdace7e3b561a5d9
yeahieaKhan <yeahieakhan2017@gmail.com>
2026-02-27 20:34:49 +0600

    updated jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,34 +1,33 @@
-
 import { pool } from "../config/db";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const loginUser = async (email: string, password: string) => {
-    const result = await pool.query(`SELECT * FROM users WHERE email =$1`, [email]);
-    if (result.rows.length === 0) {
-        return null;
-    }
-    const user = result.rows[0];
-    const match = await bcrypt.compare(password, user.password)
+  const result = await pool.query(`SELECT * FROM users WHERE email =$1`, [
+    email,
+  ]);
+  if (result.rows.length === 0) {
+    return null;
+  }
+  const user = result.rows[0];
+  const match = await bcrypt.compare(password, user.password);
 
-    if (!match) {
-        return false
-    }
-    const secret = "KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
+  if (!match) {
+    return false;
+  }
 
-    const token = jwt.sign({ name: user.name, email: user.email }, secret, {
-        expiresIn:"7d"
-    })
-    
-    console.log(token) 
-    
-    return {token,user}
+  console.log(user, match);
+  const secret = "KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
 
+  const token = jwt.sign({ name: user.name, email: user.email }, secret, {
+    expiresIn: "7d",
+  });
 
+  console.log("This is the token", token);
 
-}
-
+  return { token, user };
+};
 
 export const authService = {
-    loginUser
-}
+  loginUser,
+};
